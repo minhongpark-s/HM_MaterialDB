18ecbd7f600ec228b058a86ffe7fc17332f93028
김정훈 <hoonmin1206@gmail.com>
2022-03-10 23:18:19 +0900

    admin static 추가

--- MaterialDB/settings/prod.py
+++ MaterialDB/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-SECRET_KEY = 'django-insecure-&=#49x(9#o2#@gl92+y1v5@cgeg0pw89m+!z#@lu#nv1gib6!h'
-
 ALLOWED_HOSTS = ['3.36.148.42']
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
 
