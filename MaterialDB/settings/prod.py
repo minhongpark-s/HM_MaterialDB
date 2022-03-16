c16d317450894c73a99a705f159f102282b06b32
Ubuntu <ubuntu@ip-172-26-15-23.ap-northeast-2.compute.internal>
2022-03-16 18:24:27 +0900

    3/16

--- MaterialDB/settings/prod.py
+++ MaterialDB/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['52.79.134.252', 'hymec.kr']
+ALLOWED_HOSTS = ['13.125.122.7', 'hymec.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
